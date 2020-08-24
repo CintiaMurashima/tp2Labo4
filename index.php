8652c29a744597315fe74861b4cd2efd7c64c290
CintiaMurashima <cintiamurashima@gmail.com>
2020-08-24 17:51:27 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">      
+
+</head>
+
+<body>
+    <div class="wrapper">
+    
+        <header class="div1">
+            <h1>Header</h1> 
+        </header>
+        <nav class="nav">
+            <h1>Nav</h1> 
+        </nav>
+
+        <aside class="aside">
+        <h1>Aside</h1> 
+        </aside>
+
+        <section class="section">
+            <h1>Section<h1>
+            <article class="article">
+                <h4>Article<h4>
+                <details class="sumary"> 
+                        <sumary>
+                            <h5>Sumary</h5> 
+                        </sumary>
+                    </details>
+            </article>
+
+        </section>
+
+        <footer class="footer">
+            <h1>Footer</h1> 
+        </footer>
+    </div>
+
+</body>
+</html>
